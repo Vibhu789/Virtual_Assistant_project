c9cfb89571febc9864075101a37f7232d6edf13b
Vibhu Tyagi <129052431+Vibhu789@users.noreply.github.com>
2025-09-19 02:10:31 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,14 +12,16 @@ import Visualization from "./pages/Visualization";
 function App() {
   const {userData,setUserData}=useContext(userDataContext)
   return(
+    <HashRouter>
     <Routes>
       <Route path='/' element={(userData?.assistantImage && userData?.assistantName)? <Home/> :<Navigate to={"/customize"}/>}/>
       <Route path='/signup' element={!userData?<SignUp/>:<Navigate to={"/"}/>}/>
       <Route path='/signin' element={!userData?<SignIn/>:<Navigate to={"/"}/>}/>
       <Route path='/customize' element={userData?<Customize/>:<Navigate to={"/signup"}/>}/>
       <Route path='/customize2' element={userData?<Customize2/>:<Navigate to={"/signup"}/>}/> 
-     <Route path="/Visualization" element={<Visualization />} />
+     <Route path="/visualization" element={<Visualization />} />
     </Routes>
+    </HashRouter>
   )
 }
 
