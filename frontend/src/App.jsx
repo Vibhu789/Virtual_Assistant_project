bf46becf36bcfc2f2f7606516a156d75835bae58
Vibhu Tyagi <129052431+Vibhu789@users.noreply.github.com>
2025-09-19 01:07:33 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Customize from "./pages/Customize";
 import { userDataContext } from "./context/UserContext";
 import Home from "./pages/Home";
 import Customize2 from "./pages/Customize2";
+import GroundwaterTable from "./GroundwaterTable";
 
 
 function App() {
@@ -16,7 +17,8 @@ function App() {
       <Route path='/signup' element={!userData?<SignUp/>:<Navigate to={"/"}/>}/>
       <Route path='/signin' element={!userData?<SignIn/>:<Navigate to={"/"}/>}/>
       <Route path='/customize' element={userData?<Customize/>:<Navigate to={"/signup"}/>}/>
-      <Route path='/customize2' element={userData?<Customize2/>:<Navigate to={"/signup"}/>}/>  
+      <Route path='/customize2' element={userData?<Customize2/>:<Navigate to={"/signup"}/>}/> 
+      <Route path="/Visualization" element={<GroundwaterTable />} />
     </Routes>
   )
 }
